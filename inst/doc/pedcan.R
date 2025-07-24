15e2a958089c354aee741b17aab39a467594f9fd
Antoine BICHAT <antoine.bichat@proton.me>
2025-07-24 19:20:02 +0000

    version 0.0.3

--- inst/doc/pedcan.R
+++ inst/doc/pedcan.R
@@ -22,30 +22,28 @@ count(pedcan_expression, disease, sort = TRUE)
 
 ## -----------------------------------------------------------------------------
 rec_naive_pca <-
-  recipe(pedcan_expression) %>% 
-  update_role(-cell_line) %>% 
-  step_zv(all_numeric_predictors()) %>% 
-  step_normalize(all_numeric_predictors()) %>% 
-  step_pca(all_numeric_predictors()) %>% 
+  recipe(~., data = pedcan_expression) %>%
+  step_zv(all_numeric_predictors()) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_numeric_predictors()) %>%
   prep()
 
-rec_naive_pca %>% 
-  bake(new_data = NULL) %>% 
+rec_naive_pca %>%
+  bake(new_data = NULL) %>%
   ggplot() +
   aes(x = PC1, y = PC2, color = disease) +
   geom_point()
 
 ## -----------------------------------------------------------------------------
 rec_cv_pca <-
-  recipe(pedcan_expression) %>% 
-  update_role(-cell_line) %>% 
-  step_select_cv(all_numeric_predictors(), prop_kept = 1/4) %>% 
+  recipe(~., data = pedcan_expression) %>%
+  step_select_cv(all_numeric_predictors(), prop_kept = 1 / 4) %>%
   step_normalize(all_numeric_predictors()) %>%
   step_pca(all_numeric_predictors()) %>%
   prep()
 
-rec_cv_pca %>% 
-  bake(new_data = NULL) %>% 
+rec_cv_pca %>%
+  bake(new_data = NULL) %>%
   ggplot() +
   aes(x = PC1, y = PC2, color = disease) +
   geom_point()
